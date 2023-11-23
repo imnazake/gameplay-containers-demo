0ffcd550e4addfea687230441365bf27f2a77b11
Neoxyne <imneoxyne@gmail.com>
2023-11-23 20:32:45 +0100

    Initial commit

---
+++ Source/GCDemo/GCDemo.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GCDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GCDemo, "GCDemo" );
+
+DEFINE_LOG_CATEGORY(LogGCDemo)
+ 
