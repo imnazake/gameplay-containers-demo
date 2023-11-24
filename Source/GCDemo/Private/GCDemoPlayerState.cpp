b6e64a1554b64b9dd22f539855392c66b48e4a39
Neoxyne <imneoxyne@gmail.com>
2023-11-24 18:27:18 +0100

    Added ability system to the player state

---
+++ Source/GCDemo/Private/GCDemoPlayerState.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GCDemoPlayerState.h"
+#include "AbilitySystemComponent.h"
+
+AGCDemoPlayerState::AGCDemoPlayerState()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	// AbilitySystemComponent needs to be updated at a high frequency.
+	NetUpdateFrequency = 100.0f;
+}
+
+void AGCDemoPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGCDemoPlayerState::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	check(AbilitySystemComponent);
+	AbilitySystemComponent->InitAbilityActorInfo(this, GetPawn());
+}
+
+UAbilitySystemComponent* AGCDemoPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/GCDemo/Public/GCDemoPlayerState.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "GCDemoPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GCDEMO_API AGCDemoPlayerState : public APlayerState, public IAbilitySystemInterface 
+{
+	GENERATED_BODY()
+
+public:
+
+	AGCDemoPlayerState();
+	
+	virtual void BeginPlay() override;
+	virtual void PostInitializeComponents() override;
+
+	//~ IAbilitySystemInterface
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	//~ IAbilitySystemInterface
+
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Abilities")
+	UAbilitySystemComponent* AbilitySystemComponent;
+	
+};
