a9fa2d8c98f21e2b60a63d398896995a9d7c58a6
Neoxyne <imneoxyne@gmail.com>
2023-12-03 16:51:56 +0100

    Removed unnecessary class finders

--- Source/GCDemo/Private/GCDemoGameMode.cpp
+++ Source/GCDemo/Private/GCDemoGameMode.cpp
@@ -9,18 +9,7 @@ AGCDemoGameMode::AGCDemoGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = AGCDemoPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
+	
+	// use our custom Character class
+	DefaultPawnClass = AGCDemoCharacter::StaticClass();
 }
