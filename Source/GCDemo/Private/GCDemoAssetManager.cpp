66ec240059be99300a93d6ea4bf9b11d34867940
Neoxyne <imneoxyne@gmail.com>
2024-01-17 19:07:55 +0100

    Added a new asset manager class to handle initializing gameplay ability system global data

---
+++ Source/GCDemo/Private/GCDemoAssetManager.cpp
@@ -0,0 +1,13 @@
+﻿/**
+ * Copyright (C) 2024 Mohamed Labidi (Nazake). All Rights Reserved.
+ */
+
+#include "GCDemoAssetManager.h"
+#include "AbilitySystemGlobals.h"
+
+void UGCDemoAssetManager::StartInitialLoading()
+{
+    Super::StartInitialLoading();
+
+	UAbilitySystemGlobals::Get().InitGlobalData();
+}
---
+++ Source/GCDemo/Public/GCDemoAssetManager.h
@@ -0,0 +1,23 @@
+﻿/**
+ * Copyright (C) 2024 Mohamed Labidi (Nazake). All Rights Reserved.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "GCDemoAssetManager.generated.h"
+
+/**
+ * UGCDemoAssetManager
+ */
+UCLASS()
+class GCDEMO_API UGCDemoAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void StartInitialLoading() override;
+	
+};
